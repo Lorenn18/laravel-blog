c66c6f5189c719dfc575cf8b9154cb91dd7dc39c
Lorenn18 <lorenalexandru14@gmail.com>
2022-09-22 11:20:48 +0200

    first commit

---
+++ resources/views/layout/layout.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+        integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;400;600;700&display=swap" rel="stylesheet">
+
+    <title>Document | @yield('title')</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    @include('partials.navbar._nav')
+    @yield('content')
+</body>
+
+</html>
---
+++ resources/views/partials/navbar/_nav.blade.php
@@ -0,0 +1,27 @@
+<div class="navbar bg-black">
+    <div class="navbar-start">
+        <div class="text-white">
+            <ul class="flex space-x-7">
+                <li>
+                    <i class="fa-brands fa-square-facebook"></i>
+                </li>
+                <li>
+                    <i class="fa-brands fa-instagram"></i>
+                </li>
+                <li><i class="fa-brands fa-square-snapchat"></i></li>
+                <li><i class="fa-brands fa-flickr"></i></li>
+                <li><i class="fa-brands fa-twitter"></i></li>
+                <li><i class="fa-brands fa-linkedin-in"></i></li>
+            </ul>
+        </div>
+    </div>
+    <div class="navbar-end">
+        <button class="text-white">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+            </svg>
+        </button>
+    </div>
+</div>
